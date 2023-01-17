d05e3aad4e2a0607143a81dfbf739526b267f02f
Hritik <hritkmjth@gmail.com>
2023-01-17 14:50:20 +0545

    Extractor Updated, comments removed

--- extractor.py
+++ extractor.py
@@ -1,6 +1,5 @@
 import sys
 
-
 input_file = sys.argv[1]
 
 with open(input_file) as f:
@@ -8,17 +7,13 @@ with open(input_file) as f:
     lines = f.readlines()
     for line in lines:
         l = line.split()
-        # print(l)
         p = l[6].split("/open")
-        # print(p)
-        # print(f"{l[3]}:{p[0]}")
-    #     try:
+       
         if l[3] in out_dict.keys():
             out_dict[f'{l[3]}'].append(p[0])
         else:
             out_dict[f'{l[3]}'] = [p[0]]
-    #     except:
-    #         out_dict[f'{l[3]}'] = [p[0]]
+    
     with open("out.log","w") as o:
         for k,v in out_dict.items():
             o.writelines(f"{k}:{v}\n")
